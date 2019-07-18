4b635aabb73706717fbc0232b3cede98a289a502
squirrelzt <squirrelzt@126.com>
2019-07-18 18:04:53 +0800

    修改表单类型

--- src/component/addform/AddForm.js
+++ src/component/addform/AddForm.js
@@ -50,13 +50,15 @@ class AddForm extends Component {
             // console.log('-------------------------');
             // console.log(item);
             // console.log(formData[item]);
-            let type = formData[item].type;
-            let value = formData[item].value;
+            // let type = formData[item].type;
+            // let value = formData[item].value;
             return(
                 <Form.Item
                     label={item}
                     key={item + index}>
-                    <Input key={item + 'input'} value={value} disabled></Input>
+                    {this.props.form.getFieldDecorator(item)(
+                        <Input disabled></Input>,
+                    )}
                 </Form.Item>
             );
         });
